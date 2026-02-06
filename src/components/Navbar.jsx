edb99edc032043b067ac30c11e75b6531d084b04
johnjoshua2118 <johnjoshua2118@gmail.com>
2026-02-06 15:59:25 +0530

    Update UI with Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2">
             <img 
-              src="public\logo.png" 
+              src="/logo.png"
               alt="ExpenseIQ Logo" 
               className="w-10 h-10 object-contain"
             />
